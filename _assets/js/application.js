0a300685ed24c0459dabd991616bbf3b876f045a
David Pierce <david.dean.pierce@gmail.com>
2015-01-03 21:43:35 -0800

    Sets up basic UI and forms controllers for meal input

--- _assets/js/application.js
+++ _assets/js/application.js
@@ -3,7 +3,89 @@
 var wfd = angular.module('wfd', ['ngMaterial']);
 
 wfd.controller('wfd-app', function ($scope) {
-  $scope.message = "Hello World";
+  $scope.selectedDay = 'Monday';
+  $scope.days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+
+  // Top-level Meal Plan
+  $scope.plan = {
+    'Monday': {
+      'Breakfast' : { name: '', url: '', ingredients: [] },
+      'Lunch'     : { name: '', url: '', ingredients: [] },
+      'Dinner'    : { name: '', url: '', ingredients: [] }
+    },
+    'Tuesday': {
+      'Breakfast' : { name: '', url: '', ingredients: [] },
+      'Lunch'     : { name: '', url: '', ingredients: [] },
+      'Dinner'    : { name: '', url: '', ingredients: [] }
+    },
+    'Wednesday': {
+      'Breakfast' : { name: '', url: '', ingredients: [] },
+      'Lunch'     : { name: '', url: '', ingredients: [] },
+      'Dinner'    : { name: '', url: '', ingredients: [] }
+    },
+    'Thursday': {
+      'Breakfast' : { name: '', url: '', ingredients: [] },
+      'Lunch'     : { name: '', url: '', ingredients: [] },
+      'Dinner'    : { name: '', url: '', ingredients: [] }
+    },
+    'Friday': {
+      'Breakfast' : { name: '', url: '', ingredients: [] },
+      'Lunch'     : { name: '', url: '', ingredients: [] },
+      'Dinner'    : { name: '', url: '', ingredients: [] }
+    },
+    'Saturday': {
+      'Breakfast' : { name: '', url: '', ingredients: [] },
+      'Lunch'     : { name: '', url: '', ingredients: [] },
+      'Dinner'    : { name: '', url: '', ingredients: [] }
+    },
+    'Sunday': {
+      'Breakfast' : { name: '', url: '', ingredients: [] },
+      'Lunch'     : { name: '', url: '', ingredients: [] },
+      'Dinner'    : { name: '', url: '', ingredients: [] }
+    }
+  };
+
+  $scope.meals = [
+    'Breakfast',
+    'Lunch',
+    'Dinner'
+  ];
+});
+
+wfd.controller('meal-controller', function ($scope) {
+  var makeAccessor = function (path) {
+    return function (value) {
+      if (angular.isDefined(value)) {
+        $scope.plan[$scope.day][$scope.meal][path] = value;
+      }
+      return $scope.plan[$scope.day][$scope.meal][path];
+    };
+  };
+
+  $scope.mealName = makeAccessor('name');
+  $scope.mealUrl = makeAccessor('url');
+  $scope.mealIngredients = function (value) {
+    if (angular.isDefined(value)) {
+      $scope.plan[$scope.day][$scope.meal].ingredients = value
+        // Break input into individual items by line breaks or commas
+        .split(/,|\n|\r\n/)
+        // Clean up any extra whitespace
+        .map(function (ingredient) {
+          return ingredient.trim();
+        });
+    }
+
+    return $scope.plan[$scope.day][$scope.meal].ingredients.join('\n');
+  };
+
 });
 
 }(window.angular));
