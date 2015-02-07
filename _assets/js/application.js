9ec3fc04b419581c4db7569c365a2e5264ddaa97
David Pierce <david.dean.pierce@gmail.com>
2015-02-07 14:48:52 -0800

    Prompt user to play video rather than autoplay

--- _assets/js/application.js
+++ _assets/js/application.js
@@ -239,16 +239,22 @@ wfd.controller('meal-controller', function ($scope, socket) {
 
 });
 
-wfd.controller('wfd-welcome', function () {
-  var demoWindow = welcomeForm.querySelector('.browserChrome .content');
-  var video = document.createElement('video');
-  video.src = "/img/wfd-demo.mp4";
-  video.loop = true;
-  video.controls = true;
-  video.style.width = "100%";
-  video.oncanplay = function () {
-    demoWindow.replaceChild(video, demoWindow.firstElementChild);
-    video.play();
+wfd.controller('wfd-welcome', function ($scope) {
+  $scope.playDemo = function ($event) {
+    $event.preventDefault();
+    var demoWindow = welcomeForm.querySelector('.browserChrome .content');
+    var overlay = welcomeForm.querySelector('.browserChrome .overlay');
+    var video = document.createElement('video');
+
+    video.src = "/img/wfd-demo.mp4";
+    video.loop = true;
+    video.controls = true;
+    video.style.width = "100%";
+    video.oncanplay = function () {
+      demoWindow.replaceChild(video, demoWindow.firstElementChild);
+      video.play();
+      overlay.classList.add("isPlaying");
+    };
   };
 });
 
